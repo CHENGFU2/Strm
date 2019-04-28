a631bfbc7ce8d0520278e9c33b0c7ba589a65d6e
DivineTurbo <1229992436@qq.com>
2019-04-29 06:00:43 +0800

    Revert "Signed-off-by: Lisa <1229992436@qq.com>"
    This reverts commit e95009bf346d17a26cd8eea7f799bfcc7c71fe78.

--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-# 自定义卸载文件,示例:
-# (until [ -f /data/swapfile ]; do sleep 20; done
-# rm /data/swapfile &) &
-# exit 0
-
-(until [ -f /data/swapfile ]; do sleep 20; done
- rm /data/swapfile &) &
- exit 0
